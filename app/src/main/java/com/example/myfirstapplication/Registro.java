d09404da036e185587e13b14716d63a75f5e0688
Cami7102 <80707476+Cami7102@users.noreply.github.com>
2021-06-20 19:46:59 -0400

    pantallas principales

--- app/src/main/java/com/example/myfirstapplication/Registro.java
+++
@@ -1,9 +0,0 @@
-package com.example.myfirstapplication;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class Registro extends AppCompatActivity {
-
-    EditText campoNombre, campoContraseña;
-
-}
